a1c796c54bc9fca8b3cf2e496ef682b7c7ea3f3a
Michal Klos <michal.m.klos@gmail.com>
2018-03-14 21:20:48 +0100

    removed comments

--- PolyEngine/RenderingDevice/OpenGL/Src/GLParticleDeviceProxy.cpp
+++ PolyEngine/RenderingDevice/OpenGL/Src/GLParticleDeviceProxy.cpp
@@ -10,8 +10,6 @@ using namespace Poly;
 //---------------------------------------------------------------
 GLParticleDeviceProxy::GLParticleDeviceProxy()
 {
-	// gConsole.LogInfo("GLParticleDeviceProxy::GLParticleDeviceProxy");
-	
 	// quad with uv mapping
 	static const float vertices[] = {
 		// x,y,z, u,v
@@ -36,14 +34,10 @@ GLParticleDeviceProxy::GLParticleDeviceProxy()
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (GLvoid*)(3 * sizeof(float)));
 
-	// gConsole.LogInfo("InstancedMeshRenderingPass::Ctor sizeof(Matrix): {}, sizeof(GLfloat): {}", sizeof(Matrix), sizeof(GLfloat));
 
-	
 	glGenBuffers(1, &instanceVBO);
 	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
-	// glBufferData(GL_ARRAY_BUFFER, sizeof(float) * instancesTransform.GetSize(), instancesTransform.GetData(), GL_STATIC_DRAW);
 	glBufferData(GL_ARRAY_BUFFER, 0, NULL, GL_STATIC_DRAW);
-	// glBindBuffer(GL_ARRAY_BUFFER, 0);
 	
 	// http://sol.gfxile.net/instancing.html
 	// int pos = glGetAttribLocation((GLint)GetProgram().GetProgramHandle(), "aOffset");
@@ -56,7 +50,6 @@ GLParticleDeviceProxy::GLParticleDeviceProxy()
 	glEnableVertexAttribArray(pos2);
 	glEnableVertexAttribArray(pos3);
 	glEnableVertexAttribArray(pos4);
-	// glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
 	glVertexAttribPointer(pos1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 4 * 4, (void*)(0));
 	glVertexAttribPointer(pos2, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 4 * 4, (void*)(sizeof(float) * 4));
 	glVertexAttribPointer(pos3, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 4 * 4, (void*)(sizeof(float) * 8));
